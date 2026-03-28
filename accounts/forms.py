f902c89f27238019eae884aa3143f815cdb42e26
Saladin-B <saladeen.baalache@gmail.com>
2026-03-28 01:20:08 +0000

    added forms in account

---
+++ accounts/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser, ShopProfile
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
+
+
+class ShopOwnerRegistrationForm(UserCreationForm):
+    """Registration form for shop owners."""
+    shop_name = forms.CharField(max_length=255, required=True)
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(Column('username'), Column('email')),
+            Row(Column('password1'), Column('password2')),
+            'shop_name',
+            Submit('submit', 'Create Account', css_class='btn btn-primary w-100'),
+        )
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.role = 'owner'
+        if commit:
+            user.save()
+            ShopProfile.objects.create(
+                owner=user,
+                shop_name=self.cleaned_data['shop_name']
+            )
+        return user
