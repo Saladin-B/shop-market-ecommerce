3490c9c26d541b43cb588dc54724fddf2d51050a
Saladin-B <saladeen.baalache@gmail.com>
2026-03-30 16:13:51 +0100

    Fix database env var names for Heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = "my_project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("shop_management_system"),
+        "NAME": config("DB_NAME"),
         "USER": config("DB_USER"),
         "PASSWORD": config("DB_PASSWORD"),
         "HOST": config("DB_HOST", default="localhost"),
