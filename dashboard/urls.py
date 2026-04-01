b65017240b9798ee61ac22843a3656f9b293e9d4
Saladin <saladeen.baalache@gmail.com>
2026-04-01 22:10:17 +0100

    Fix dashboard URL path

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("dashboard/", views.home, name="dashboard"),  # make sure dashboard/views.py has home()
+    path("", views.home, name="home"),
 ]
