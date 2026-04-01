658a76e48fa1fb78dd1efa8c03dc7641770d9057
Saladin <saladeen.baalache@gmail.com>
2026-04-01 21:45:57 +0100

    fix redirect login and dashboard url

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),  # make sure dashboard/views.py has home()
+    path("dashboard/", views.home, name="home"),  # make sure dashboard/views.py has home()
 ]
