5a9247969ab566bb4bb6dc2373c4ccd1a72111d4
Saladin <saladeen.baalache@gmail.com>
2026-04-05 19:53:20 +0100

    Fix media file serving in production using Django serve view

--- my_project/urls.py
+++ my_project/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -28,9 +29,11 @@ urlpatterns = [
     path("messaging/", include("messaging.urls")),
 ]
 
-# Serve media files in development
+# Serve media files
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
-    # Production: Also serve media files (for local storage)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Production: Serve media files using Django's serve view
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
